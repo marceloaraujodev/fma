c531ba846cc08d9812ddd80876651da9ad731838
codetrsh <134286416+codetrsh@users.noreply.github.com>
2026-03-30 11:44:41 -0300

    max edge

--- app/layout.js
+++ app/layout.js
@@ -1,60 +1,69 @@
-import localFont from "next/font/local";
-import { Inter, Outfit, Roboto, Work_Sans } from "next/font/google"; 
-import Nav from "./components/Nav";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
-import "./globals.css";
+import localFont from 'next/font/local';
+import { Inter, Outfit, Roboto, Work_Sans } from 'next/font/google';
+import Nav from './components/Nav';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import './globals.css';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 const workSans = Work_Sans({
-  subsets: ["latin"],
+  subsets: ['latin'],
   // variable: "--font-work-sans", // You can use a custom CSS variable
-  weight: "400", // Specify the weight
+  weight: '400', // Specify the weight
 });
 
 const outfit = Outfit({
-  subsets: ["latin"],
+  subsets: ['latin'],
   // variable: "--font-roboto", // You can use a custom CSS variable
-  weight: "400", // Specify the weight
+  weight: '400', // Specify the weight
 });
 
 export const metadata = {
-  title: "F.M.A - Fashion Marketing Agency",
-  description: "Fashion Marketing Agency",
+  title: 'F.M.A - Fashion Marketing Agency',
+  description: 'Fashion Marketing Agency',
 };
 
-const error = true;
+let error = false;
+
 export default function RootLayout({ children }) {
-  // if (error) {
-  //   return (
-  //     <html lang="en">
-  //       <body style={{ fontWeight: "bold", fontSize: "24px" }}>
-  //         502 Service Temporarily Unavailable
-  //       </body>
-  //     </html>
-  //   );
-  // }
+  const rand = Math.random();
+  if (rand < 0.6) {
+    error = true;
+  }
+
+  if (error) {
+    return (
+      <html lang="en">
+        <body
+          style={{
+            fontWeight: 'bold',
+            fontSize: '24px',
+            backgroundColor: '#fff',
+            color: '#000',
+          }}>
+          502 Service Temporarily Unavailable
+          <p style={{fontSize: '14px', fontWeight: 'normal'}}>Max Edge Requests reached.</p>
+        </body>
+      </html>
+    );
+  }
 
   return (
     <html lang="en">
-      <body
-        className={`${outfit.className} ${workSans.className} antialiased`}
-      >
+      <body className={`${outfit.className} ${workSans.className} antialiased`}>
         <Nav />
-        <main className='content'>
-        <MainContent>
-          {children}
-        </MainContent>
+        <main className="content">
+          <MainContent>{children}</MainContent>
         </main>
         <Footer />
       </body>
